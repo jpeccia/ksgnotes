d5301f04a4d9c15b99564622dd38a8c980156779
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-10-25 15:56:14 -0300

    add buttontext component

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  background: none;
+  color: ${({ theme }) => theme.COLORS.PURPLE};
+
+  border: none;
+  font-size: 16px;
+`;
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,10 @@
 import { Container, Links } from './styles';
 
-import { Header } from '../../components/Header';
-import { Button } from '../../components/Button';
-import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 
 
 
@@ -12,6 +13,8 @@ export function Details(){
   return (
     <Container>
     <Header />
+
+    <ButtonText title="Excluir nota" />
 
     <Section title="Links úteis">
         <Links>
