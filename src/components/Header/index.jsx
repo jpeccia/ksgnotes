1d85fc47775b64f59a169a4ad55994ca9b2a98d1
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-10-25 15:27:18 -0300

    add profile img

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,21 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 export function Header(){
     return (
         <Container>
-            
+            <Profile>
+                <img src="https://github.com/jpeccia.png" 
+                alt='Foto do Usuário'
+                />
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>João Otávio Peccia</strong>
+                </div>
+
+
+            </Profile>
+
         </Container>
     )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,19 +1,45 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-    grid-area: header;
+  grid-area: header;
 
-    height: 105px;
-    width: 100%;
+  height: 105px;
+  width: 100%;
 
-    border-bottom-width: 1px;
-    border-bottom-style: solid;
-    border-bottom-color: ${({theme}) => theme.COLORS.BACKGROUND_700};
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0 80px;
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+  }
+
+  > div {
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
 
-    padding: 0 80px;
+    span {
+      font-size: 14px;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
 
-    background: red;
+    strong {
+      font-size: 18px;
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
 `;
