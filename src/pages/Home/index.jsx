69b3210c62400e4302801f5f5b565f6690f1fd34
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-10-25 16:22:09 -0300

    create home and apply grid

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,29 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+
+import { Header } from '../../components/Header'
+
+export function Home(){
+    return(
+        <Container>
+            <Brand>
+            <h1>K$GNotes</h1>
+            </Brand>
+
+            <Header />
+
+            <Menu>
+
+            </Menu>
+
+            <Search>
+
+            </Search>
+
+            <Content>
+
+            </Content>
+
+            <NewNote />
+        </Container>
+    )
+}
