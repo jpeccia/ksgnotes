bb552ab700914db67aab3c75a52590a0de625979
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-10-25 14:41:36 -0300

    add global style

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
@@ -9,7 +10,8 @@ import { Details } from './pages/Details'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme = {theme}>
-    <Details />
+      <GlobalStyles />
+      <Details />
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,5 +1,3 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  background: red;
-`;
+export const Container = styled.div``;
