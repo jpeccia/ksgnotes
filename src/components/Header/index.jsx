9bc20960eac80105f98533a4d690e58bb9b1cc17
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-10-25 15:33:24 -0300

    add react icons

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
-import { Container, Profile } from './styles'
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from './styles'
 
 export function Header(){
     return (
@@ -12,9 +13,11 @@ export function Header(){
                     <span>Bem-vindo</span>
                     <strong>João Otávio Peccia</strong>
                 </div>
-
-
             </Profile>
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
 
         </Container>
     )
