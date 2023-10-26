87dbadf55bc8f0e8a2c72f568cf76307dc33fbd5
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-10-26 16:09:46 -0300

    fix: fix noteitem styles

--- src/components/NoteItem/index.jsx
+++ src/components/NoteItem/index.jsx
@@ -1,27 +1,26 @@
-import { FiPlus, FiX } from 'react-icons/fi';
+import { FiPlus, FiX } from 'react-icons/fi'
 
-import { Container } from './styles';
+import { Container } from './styles'
 
-export function NoteItem({ isNew, value, onClick, ...rest}){
-    return(
-        <Container isNew={isNew}>
-            <input 
-            type="text" 
-            value={value} 
-            readOnly={!isNew}
-            {...rest} 
-            />
+export function NoteItem({ isNew, value, onClick, ...rest }) {
+  return (
+    <Container $isnew={isNew}>
+      <input
+        type="text"
+        value={value}
+        readOnly={!isNew}
+        {...rest}
+      />
 
-            <button 
-            type="button"
-            onClick={onClick}
-            className={isNew ? 'button-add' : 'button-delete'}
-            >
-                { isNew ? <FiPlus /> : <FiX />}
-            </button>
-
-        </Container>
-    )
+      <button
+        type="button"
+        onClick={onClick}
+        className={isNew ? 'button-add' : 'button-delete'}
+      >
+        {isNew ? <FiPlus /> : <FiX />}
+      </button>
+    </Container>
+  )
 }
 
 NoteItem.propTypes;
--- src/components/NoteItem/styles.js
+++ src/components/NoteItem/styles.js
@@ -4,12 +4,12 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
 
-  background-color: ${({ theme, isNew }) =>
-    isNew ? "transparent" : theme.COLORS_BACKGROUND_900};
-  color: ${({ theme }) => theme.COLORS_GRAY_300};
+  background-color: ${({ theme, $isnew }) =>
+    $isnew ? "transparent" : theme.COLORS.BACKGROUND_900};
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
 
-  border: ${({ theme, isNew }) =>
-    isNew ? `1px dashed ${theme.COLORS_}` : "none"};
+  border: ${({ theme, $isnew }) =>
+    $isnew ? `1px dashed ${theme.COLORS.GRAY_300}` : "none"};
 
   margin-bottom: 8px;
   border-radius: 10px;
@@ -25,7 +25,7 @@ export const Container = styled.div`
   }
 
   .button-add {
-    color: ${({ theme }) => theme.COLORS.WHITE};
+    color: ${({ theme }) => theme.COLORS.PURPLE};
   }
 
   > input {
