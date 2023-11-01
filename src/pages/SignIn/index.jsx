5efa1c02a073c52b1dfc8d1733ebf01ab1519763
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-01 14:52:55 -0300

    feat: auth hook

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import { FiMail, FiLock } from 'react-icons/fi';
-
 import { Link } from 'react-router-dom';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 import { Container, Form, Background } from './styles';
+import { useAuth } from '../../hooks/auth';
 
 
 export function SignIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { signIn } = useAuth();
+
+    function handleSignIn(){
+        signIn({ email, password });
+    }
+
     return(
         <Container>
             <Form>
@@ -21,14 +31,16 @@ export function SignIn(){
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
                  <Input 
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Entrar"/>
+                <Button title="Entrar" onClick={handleSignIn}/>
                 <Link to="/register">
                     Criar conta
                 </Link>
