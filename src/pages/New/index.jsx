e5fcb15d16bf22280a42f8f4e63a6cb93d8dcdf7
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-02 13:49:49 -0300

    feat: note links add

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { Header } from '../../components/Header'
@@ -10,6 +11,14 @@ import { Button } from '../../components/Button'
 import { Container, Form } from './styles'
 
 export function New(){
+    const [links, setLinks] = useState([]);
+    const [newLink, setNewLink] = useState("");
+
+    function handleAddLink(){
+        setLinks(prevState=> [...prevState, newLink])
+        setNewLink("");
+    }
+
     return (
         <Container>
             <Header />
@@ -25,8 +34,21 @@ export function New(){
                     <Textarea placeholder="Observações"/>
 
                     <Section title="Links úteis">
-                        <NoteItem value="https>//teste.com"/>
-                        <NoteItem isNew placeholder="Novo Link"/>
+                        {
+                            links.map((link, index) => (
+                                <NoteItem
+                                key={String(index)}
+                                value={link}
+                                onClick={() => { }}
+                                />  
+                            ))
+                        }
+                        <NoteItem isNew 
+                        placeholder="Novo Link"
+                        value={newLink}
+                        onChange={e => setNewLink(e.target.value)}
+                        onClick={handleAddLink}
+                        />
                     </Section>
 
                     <Section title="Marcadores">
