c94baa151d3c4e4d12fbc4d59790c43ae0763bac
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-02 14:41:42 -0300

    feat: add select and unselect tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,42 @@
-import { FiPlus } from 'react-icons/fi'
+import { useState, useEffect } from 'react';
+import { FiPlus } from 'react-icons/fi';
 
-import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+import { api } from '../../services/api';
 
-import { Input } from '../../components/Input'
-import { Note } from '../../components/Note'
-import { Header } from '../../components/Header'
-import { Section } from '../../components/Section'
-import { ButtonText } from '../../components/ButtonText'
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+
+import { Input } from '../../components/Input';
+import { Note } from '../../components/Note';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 
 
 export function Home(){
+    const [tags, setTags] = useState([]);
+    const [tagsSelected, setTagsSelected] = useState([]);
+
+    function handleTagSelected(tagName){
+        const alreadySelected = tagsSelected.includes(tagName);
+
+        if (alreadySelected) {
+            const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+            setTagsSelected(filteredTags);
+        } else {
+            setTagsSelected(prevState => [...prevState, tagName]);
+        }
+
+    }
+
+    useEffect(() =>{
+        async function fetchTags(){
+            const response = await api.get("/tags");
+            setTags(response.data)
+        }
+
+        fetchTags()
+    }, [])
+
     return(
         <Container>
             <Brand>
@@ -19,9 +46,24 @@ export function Home(){
             <Header />
 
             <Menu>
-                <li><ButtonText title="Todos" isActive/></li>
-                <li><ButtonText title="React"/></li>
-                <li><ButtonText title="Nodejs"/></li>
+            <li>
+                <ButtonText 
+                title="Todos" 
+                onClick={() => handleTagSelected("all")}
+                isActive={tagsSelected.length === 0}
+                />
+                </li>
+                {
+                    tags && tags.map(tag => (
+                        <li key={String(tag.id)}>
+                            <ButtonText 
+                            title={tag.name}
+                            onClick={() => handleTagSelected(tag.name)}
+                            isActive={tagsSelected.includes(tag.name)}
+                            />
+                            </li>
+                    ))
+                }
             </Menu>
 
             <Search>
