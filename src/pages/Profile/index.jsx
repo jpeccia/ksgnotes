bfb86773c7d31a60164cfa46b70ac7c7ed7fa7d4
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-07 16:44:26 -0300

    fix: fix useravatar update when change name

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -29,13 +29,15 @@ export function Profile(){
     }
 
     async function handleUpdate(){
-        const user = {
+        const updated = {
             name,
             email,
             password: passwordNew,
             old_password: passwordOld,
         }
-        await updateProfile({ user, avatarFile })
+
+        const userUpdated = Object.assign(user, updated)
+        await updateProfile({ user: userUpdated, avatarFile })
     }
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
