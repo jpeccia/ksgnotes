2d81454bc9ff25959de0b56c7a507dd18dd9d06f
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-07 14:17:32 -0300

    feat: note open

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
 
 import { api } from '../../services/api';
@@ -18,6 +19,8 @@ export function Home(){
     const [tagsSelected, setTagsSelected] = useState([]);
     const [notes, setNotes] = useState([]);
 
+    const navigate = useNavigate();
+
     function handleTagSelected(tagName){
         if(tagName === "all"){
             return setTagsSelected([]);
@@ -32,6 +35,10 @@ export function Home(){
             setTagsSelected(prevState => [...prevState, tagName]);
         }
 
+    }
+
+    function handleDetails(id){
+        navigate(`/details/${id}`);
     }
 
     useEffect(() =>{
@@ -84,7 +91,7 @@ export function Home(){
                  <Input 
                  
                  placeholder="Pesquisar pelo titulo"
-                 onChange={e => setSearch(e.target.value)}
+                 onChange={(e) => setSearch(e.target.value)}
                  />
             </Search>
 
@@ -95,6 +102,7 @@ export function Home(){
                                 <Note 
                                  key={String(note.id)}
                                  data={note}
+                                 onClick={() => handleDetails(note.id)}
                                 />
                             ))
                             }
