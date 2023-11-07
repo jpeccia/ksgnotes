11195774c8cdf316afd0541a26ad8c0648020d2f
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2023-11-07 16:53:03 -0300

    feat: logout redirect

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,9 +5,16 @@ import avatarPlaceholder from '../../assets/avatar_placeholder.png';
 import { api } from '../../services/api';
 
 import { Container, Profile, Logout } from './styles'
+import { useNavigate } from 'react-router-dom';
 
 export function Header(){
     const { signOut, user } = useAuth();
+    const navigation = useNavigate();
+
+    function handleSignOut(){
+        navigation("/");
+        signOut();
+    }
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
@@ -24,7 +31,7 @@ export function Header(){
                 </div>
             </Profile>
 
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine/>
             </Logout>
 
